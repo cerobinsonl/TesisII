929e8885a65d58c779ecfed80f235babad9ca9ec
cerobinsonl <60712960+cerobinsonl@users.noreply.github.com>
2023-08-02 21:30:42 -0500

    Update evaluate_model_tf2.py

--- evaluate_model_tf2.py
+++ evaluate_model_tf2.py
@@ -1,13 +1,13 @@
 import time
 import numpy as np
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 from sklearn.metrics import confusion_matrix, precision_score, f1_score, recall_score
 import argparse
 import pickle
 
 def evaluate_model_tf(model_path, x_test, y_test):
     # Load the TFLite model and allocate tensors.
-    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter = tflite.Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
 
     input_index = interpreter.get_input_details()[0]["index"]
